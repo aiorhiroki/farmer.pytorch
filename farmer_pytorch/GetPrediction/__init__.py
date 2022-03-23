74ca4965a5f82c639892d2e5085d4bb0b86f41fb
hiroki <1234defgsigeru@gmail.com>
2022-03-23 11:39:47 +0900

    refactor

--- farmer_pytorch/GetPrediction/__init__.py
+++
@@ -1,3 +0,0 @@
-from .get_prediction_abc import GetPredictionABC
-
-__all__ = ['GetPredictionABC']
--- quickstart/train_seg.py
+++ quickstart/train_seg.py
@@ -1,22 +1,17 @@
-from farmer_pytorch.GetAnnotation import GetAnnotationABC, get_annotation_fn
-from farmer_pytorch.GetDataset import GetDatasetSgmABC
-from farmer_pytorch.GetOptimization import GetOptimizationABC, optimization_fn
-
+import farmer_pytorch as fmp
 import segmentation_models_pytorch as smp
 import albumentations as albu
 import torch
 
 
-class GetAnnotationImp(GetAnnotationABC):
+class GetAnnotationImp(fmp.GetAnnotationABC):
     target = "./seg_data/CamVid"
-
     img_dir_train = "train"
     label_dir_train = "trainannot"
-    get_train_fn = get_annotation_fn.seg_case_direct
-
+    get_train_fn = fmp.readers.seg_case_direct
     img_dir_val = "val"
     label_dir_val = "valannot"
-    get_val_fn = get_annotation_fn.seg_case_direct
+    get_val_fn = fmp.readers.seg_case_direct
 
     """
     @classmethod
@@ -27,7 +22,7 @@ class GetAnnotationImp(GetAnnotationABC):
     """
 
 
-class DatasetImp(GetDatasetSgmABC):
+class DatasetImp(fmp.GetDatasetSgmABC):
     class_values = [8]
     train_trans = albu.Compose(
         [albu.augmentations.geometric.resize.Resize(256, 512),
@@ -48,7 +43,7 @@ class DatasetImp(GetDatasetSgmABC):
     """
 
 
-class GetOptimizationImp(GetOptimizationABC):
+class GetOptimizationImp(fmp.GetOptimizationABC):
     batch_size = 16
     epochs = 10
     lr = 0.001
@@ -57,7 +52,7 @@ class GetOptimizationImp(GetOptimizationABC):
     model = smp.FPN(encoder_name="efficientnet-b7", encoder_weights="imagenet",
                     activation="sigmoid", in_channels=3, classes=1,)
     loss_func = smp.losses.DiceLoss('binary')
-    metric_obj = optimization_fn.Fscore
+    metric_func = fmp.metrics.Dice()
     result_dir = "results/quickstart"
 
     """
